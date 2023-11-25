b79c9e9427c4ededf365c8de1c0afa56563ddc4c
Carolina Martínez <crisalidacar@gmail.com>
2023-11-24 21:30:09 -0500

    unificación del menú en mobile

--- Desktop/trabajos de caro/proyecto de curso frontend/main.js
+++ Desktop/trabajos de caro/proyecto de curso frontend/main.js
@@ -1,8 +1,15 @@
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuEmail = document.querySelector('.email');
-menuEmail.addEventListener('click', activarDesktopMenu);
+const mobileMenu = document.querySelector('.menu-mobile');
+const burguerMenu = document.querySelector('.menu-hamburguesa')
 
+menuEmail.addEventListener('click', toggleDesktopMenu);
+burguerMenu.addEventListener('click', toggleMobileMenu);
 
-function activarDesktopMenu() {
+function toggleDesktopMenu() {
   desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+  mobileMenu.classList.toggle('inactive');
 }
